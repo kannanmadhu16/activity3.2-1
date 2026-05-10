8f30f5eecac1cd95e54390999220548a9af867f9
kannanmadhu <kannanmadhu16@gmail.com>
2026-05-10 20:25:20 +0800

    adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate-bucket " # Change this
+    key    = "madhu-activity3.l2-s3"  # Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix = "s3-tf-madhu" # Set your bucket name here
+}
